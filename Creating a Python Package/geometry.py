c708c544e1e7b8dc33ddf762ecb06c23b6712604
JsExplorer <93700857+JsExplorer@users.noreply.github.com>
2021-12-08 23:56:46 +0800

    Add files via upload
    Basic of Python Package

---
+++ Creating a Python Package/geometry.py
@@ -0,0 +1,13 @@
+import math
+def area_of_rectangle(length, width):
+    """
+    This function returns the area of rectangle with given length and width
+    """
+    return length * width
+
+
+def area_of_circle(radius):
+    """
+    This function returns the area of circle with given radius
+    """
+    return math.pi * (radius ** 2)
